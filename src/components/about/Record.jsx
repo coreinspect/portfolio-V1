1f092d407d89c9b9f14465326e361d67a3f65428
Jury Gregorio Jr <jury.devs@gmail.com>
2024-06-11 15:45:41 +0800

    Perform Bug Resolving in Records

--- src/components/about/Record.jsx
+++ src/components/about/Record.jsx
@@ -6,17 +6,6 @@ const Records = () => {
   const countersRef = useRef([]);
   const sectionRef = useRef(null);
 
-  const isElementInViewport = (el) => {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  };
-
   const countUp = (el, max) => {
     let current = 0;
     const increment = max / 200;
